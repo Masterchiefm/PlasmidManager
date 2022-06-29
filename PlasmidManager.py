4fb81ee8e0a1fd487ff05a38e389b00cd5548168
moqiq <moqiqin@live.com>
2022-06-29 12:52:46 +0800

    Changes

--- PlasmidManager.py
+++ PlasmidManager.py
@@ -96,7 +96,7 @@ class MyMainWin(QMainWindow, Ui_MainWindow):
 
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
-        latestInfo = get("https://api.github.com/repos/Masterchiefm/PlasmidManager/releases/latest", timeout=2,  headers= headers)
+        latestInfo = get("https://gitee.com/api/v5/repos/MasterChiefm/PlasmidManager/releases/latest", timeout=2,  headers= headers)
         print(latestInfo)
         try:
             info = latestInfo.text
